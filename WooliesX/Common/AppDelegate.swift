da54347c6063db7684deee3c170e507a6f563597
Jay Salvador <jay.andrae.salvador@gmail.com>
2020-04-23 02:27:11 +1000

    Change deployment target to iOS 12

--- WooliesX/Common/AppDelegate.swift
+++ WooliesX/Common/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var isMockData: Bool = false
     
+    var window: UIWindow?
+    
     class var shared: AppDelegate? {
         
         return UIApplication.shared.delegate as? AppDelegate
@@ -28,21 +30,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.isMockData = true
         }
         
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "NavigationViewController")
+
+        self.window?.rootViewController = initialViewController
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 }
